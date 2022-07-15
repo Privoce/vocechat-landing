5ba5498a7ae8d096d10c477b4d2d08b28fb820aa
Tristan Yang <yanggc888@163.com>
2022-07-15 21:15:53 +0800

    feat: og and titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,73 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>VoceChat</title>
+        <title>VoceChat Website</title>
         <meta name="description" content="Welcome to vocechat" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#527ff1" />
+        <meta name="description" content="Your private chat APP" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content={"privoce1"} key="twhandle" />
+
+        {/* Open Graph */}
+        <meta property="og:url" content={"https://voce.chat"} key="ogurl" />
+        <meta property="og:image" content={"/android-chrome-512x512.png"} key="ogimage" />
+        <meta property="og:site_name" content={"VoceChat"} key="ogsitename" />
+        <meta property="og:title" content={"VoceChat"} key="ogtitle" />
+        <meta property="og:description" content={"Your perivate chat APP"} key="ogdesc" />
+        <link
+          href="/splash/iphone5_splash.png"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/iphone6_splash.png"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/iphoneplus_splash.png"
+          media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/iphonex_splash.png"
+          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/iphonexr_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/iphonexsmax_splash.png"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/ipad_splash.png"
+          media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/ipadpro1_splash.png"
+          media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/ipadpro3_splash.png"
+          media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
+        <link
+          href="/splash/ipadpro2_splash.png"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+          rel="apple-touch-startup-image"
+        />
       </Head>
 
       <main>
