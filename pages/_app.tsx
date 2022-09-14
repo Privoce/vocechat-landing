db565351e9efb103391093003e6aca4bdc96e34f
Tristan Yang <yanggc888@163.com>
2022-09-14 21:24:12 +0800

    feat: add google gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-D6S2F7X8YR`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-D6S2F7X8YR');
+          `
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
