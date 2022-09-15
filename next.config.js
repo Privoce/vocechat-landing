f6b4790b7434678672bce7e4180d95cd1831cb20
Tristan Yang <yanggc888@163.com>
2022-09-15 08:59:54 +0800

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental:{
-    images:{
-      allowFutureImage:true
-    }
-  }
+  // experimental:{
+  //   images:{
+  //     allowFutureImage:true
+  //   }
+  // }
 }
 
 module.exports = nextConfig
