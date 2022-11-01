0fcc5f7a16a7a434ba90aed387ce91483bdd27ca
Tristan Yang <yanggc888@163.com>
2022-11-01 10:03:19 +0800

    feat: add files for app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
