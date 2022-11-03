e8092a7eec9fc5e45e6c0be57832b1c802014d25
hdsuperman <zhd.superman@gmail.com>
2022-11-03 19:23:32 +0800

    style: change page width/height to 100%

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <body className="selection:bg-primary-300 selection:text-primary-25">
         <Main />
         <aside
