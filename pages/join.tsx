ff19c8700b5bb57cb90bbf93d190587e85c41388
Tristan Yang <yanggc888@163.com>
2022-11-09 12:08:12 +0800

    feat: update APP pages

--- pages/join.tsx
+++ pages/join.tsx
@@ -12,8 +12,11 @@ const Index = (props: Props) => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-primary-50">
         <ul className="flex flex-col justify-center items-center p-4">
           <li className="mb-4 text-2xl font-bold"> Redirecting...</li>
-          {link && <li className="text-center">
-            <a className="underline text-blueLight-600 text-center w-full break-all" href={link}>{link}</a>
+          {link && <li className="text-gray-600 text-center w-full break-all flex flex-col gap-2">
+            <span>
+              {link}
+            </span>
+            <i className="text-gray-400 not-italic text-xs">You can also copy this url and paste it in VoceChat App</i>
           </li>}
         </ul>
 
