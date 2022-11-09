12f1b1dcca58d46031bebb33cd27ffd5acde43b4
Tristan Yang <yanggc888@163.com>
2022-11-09 12:03:25 +0800

    feat: update APP pages

--- pages/download.tsx
+++ pages/download.tsx
@@ -3,17 +3,20 @@ import { useState, useEffect } from "react";
 import { useRouter } from 'next/router'
 
 import Head from "../components/Head";
+type Download = { link: string, icon: string }
 type Props = {};
-// /img/icon.app.google.play.png
 const Index = (props: Props) => {
     const router = useRouter()
-    const [download, setDownload] = useState<{ link: string, icon: string } | null>(null);
+    const [download, setDownload] = useState<Download | Download[] | null>(null);
     useEffect(() => {
         const isAndroid = typeof window !== 'undefined' ? navigator.userAgent.match(/Android/i) : false;
-        setDownload(isAndroid ? ({
+        setDownload(isAndroid ? ([{
             link: "https://play.google.com/store/apps/details?id=com.privoce.vocechatclient",
             icon: "/img/icon.app.google.play.png"
-        }) : ({
+        }, {
+            link: "https://s.voce.chat/vocechat.android.apk",
+            icon: "/img/icon.app.apk.png"
+        }]) : ({
             link: "https://apps.apple.com/app/vocechat/id1631779678",
             icon: "/img/icon.app.ios.png"
         }))
@@ -30,7 +33,12 @@ const Index = (props: Props) => {
                 </div>
                 <div className="flex flex-col items-center w-72">
                     <h1 className="mt-8 text-2xl font-bold text-center">Start by downloading VoceChat mobile app</h1>
-                    {download ? <a href={download.link} target="_blank" rel="noopener noreferrer" className="mt-14 mb-16">
+                    {download ? Array.isArray(download) ? <ul className="mt-14 mb-12"> {download.map(d => {
+                        const { link, icon } = d
+                        return <li key={link}><a href={link} target="_blank" rel="noopener noreferrer" >
+                            <img alt="App Download Icon" src={icon} className="max-w-[80%] h-auto m-auto mb-4" />
+                        </a></li>
+                    })}</ul> : <a href={download.link} target="_blank" rel="noopener noreferrer" className="mt-14 mb-16">
                         <img alt="App Download Icon" src={download.icon} className="max-w-[80%] h-auto m-auto" />
                     </a> : null}
                     {link && <p className="text-md text-gray-600">Have the app already? <a href={`https://voce.chat/join?magic_link=${link}`} className="text-blueLight-600">Open VoceChat</a> </p>}
--- pages/join.tsx
+++ pages/join.tsx
@@ -1,13 +1,22 @@
+import { useRouter } from "next/router";
 import React from "react";
 import Head from "../components/Head";
 type Props = {};
 
 const Index = (props: Props) => {
+  const router = useRouter()
+  const link = decodeURIComponent(router.query.magic_link as string ?? "")
   return (
     <>
       <Head />
       <main className="flex min-h-screen flex-col items-center justify-center bg-primary-50">
-        Redirecting...
+        <ul className="flex flex-col justify-center items-center p-4">
+          <li className="mb-4 text-2xl font-bold"> Redirecting...</li>
+          {link && <li className="text-center">
+            <a className="underline text-blueLight-600 text-center w-full break-all" href={link}>{link}</a>
+          </li>}
+        </ul>
+
       </main>
     </>
   );
