f065db5601f9076f35af5e1f588c74ebfbd21dbe
Tristan Yang <yanggc888@163.com>
2023-03-16 12:20:03 +0800

    feat: add secret to license

--- pages/license.tsx
+++ pages/license.tsx
@@ -1,22 +1,22 @@
 import React, { FormEvent, useState } from "react";
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 type Props = {};
-const KEY_TOKEN = "LEN_TOKEN";
-const localToken = typeof window !== "undefined" ? (window.localStorage.getItem(KEY_TOKEN) || "") : ""
+const KEY_SECRET = "LEN_SECRET";
+const localSecret = typeof window !== "undefined" ? (window.localStorage.getItem(KEY_SECRET) || "") : ""
 const Index = (props: Props) => {
-    const [token, setToken] = useState(localToken);
+    const [secret, setSecret] = useState(localSecret);
     const [license, setLicense] = useState("")
     const [generating, setGenerating] = useState(false)
     const [copied, setCopied] = useState(false)
     const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
         evt.preventDefault();
         const formEle = evt.currentTarget;
-        const tokenVal = formEle.token?.value;
-        if (tokenVal) {
-            setToken(tokenVal);
-            localStorage.setItem(KEY_TOKEN, tokenVal)
+        const secretVal = formEle.secret?.value;
+        if (secretVal) {
+            setSecret(secretVal);
+            localStorage.setItem(KEY_SECRET, secretVal)
             return
         } else {
             if (!formEle.checkValidity()) {
@@ -31,16 +31,20 @@ const Index = (props: Props) => {
         })
         console.log(body);
         try {
+            setLicense("")
             setGenerating(true)
-            const resp = await axios.post(`https://vera.nicegoodthings.com/vocechat/landing/license`, { token, data: body })
+            const resp = await axios.post(`https://vera.nicegoodthings.com/vocechat/landing/license`, { secret, data: body })
             console.log("resp", resp.data);
             setLicense(resp.data?.license ?? "")
             setGenerating(false)
         } catch (err) {
             setGenerating(false)
-            setLicense("error")
-            console.error(err);
-            alert("API Error")
+            if ((err as AxiosError).response?.status == 401) {
+                alert("Invalid Secret!")
+            } else {
+                alert("API Error!")
+            }
+            // console.error("error", err.response.status);
         }
     }
     const handleCopy = () => {
@@ -50,27 +54,27 @@ const Index = (props: Props) => {
         }, 2000);
     }
     const handleReset = () => {
-        localStorage.removeItem(KEY_TOKEN);
-        setToken("")
+        localStorage.removeItem(KEY_SECRET);
+        setSecret("")
     }
     const inputClass = "text-lg p-2 border border-solid border-gray-100 rounded"
     return (
         <main className="w-screen h-screen bg-gray-50 flex flex-col justify-center items-center text-2xl">
-            <h1 className="text-4xl font-bold mb-10 flex items-center gap-2">
-                <span>License Generator</span>
-                {token ? <button onClick={handleReset} className="bg-red-500 text-white px-4 py-1 rounded-full text-xs">Reset</button> : null}
+            <h1 className="text-xl md:text-4xl font-bold mb-2 md:mb-10 flex flex-col items-center gap-2">
+                <span>VoceChat License Generator</span>
+                {secret ? <button onClick={handleReset} className="bg-red-500 text-white px-4 py-1 rounded-full text-xs">Reset Secret</button> : null}
             </h1>
             <form action="" onSubmit={handleSubmit} className="flex flex-col justify-start gap-2">
-                {token ? <>
+                {secret ? <>
                     <input type="text" required className={inputClass} name="domain" placeholder="Domain" />
                     <input type="date" required className={inputClass} name="expiry_at" placeholder="Expire Date" />
                     <input type="number" required className={inputClass} name="user_limit" placeholder="User Limit" />
                 </>
-                    : <input name="token" required type="text" className={inputClass} placeholder="License Generate Token"></input>}
-                <button type="submit" disabled={generating} className="p-2 rounded-full bg-success-700 text-white mt-6 disabled:bg-gray-400 disabled:cursor-wait">{token ? generating ? `Generating` : `Generate` : "Set Token"}</button>
+                    : <input name="secret" required type="text" className={inputClass} placeholder="Input Secret Please"></input>}
+                <button type="submit" disabled={generating} className="p-1 md:p-2 rounded-full bg-success-700 text-white mt-2 md:mt-6 disabled:bg-gray-400 disabled:cursor-wait">{secret ? generating ? `Generating` : `Generate` : "Set Secret"}</button>
             </form>
-            {token && license && <>
-                <code className="bg-gray-100 text-xs p-3 rounded-md mt-10 break-words max-w-3xl">
+            {secret && license && <>
+                <code className="bg-gray-100 text-xs p-3 rounded-md mt-2 md:mt-10 break-words w-full md:max-w-3xl max-h-32 md:max-h-64 overflow-y-scroll">
                     {license}
                 </code>
                 <CopyToClipboard text={license} onCopy={handleCopy}>
